f7830c2d41007dd0dd703cfe8ab9fa8a29f9e049
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-09-28 16:20:14 +0500

    add

--- src/app/components/layout/header/logo/index.tsx
+++ src/app/components/layout/header/logo/index.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC = () => {
   return (
     <Link href="/">
       <Image
-        src="/images/logo/hayat-builder-logo.jpg"
+        src="/images/logo/hayat-builder-logo.svg"
         alt="logo"
         width={160}
         height={50}
@@ -15,7 +15,7 @@ const Logo: React.FC = () => {
         className='dark:hidden'
       />
       <Image
-        src="/images/logo/hayat-builder-logo.jpg"
+        src="/images/logo/hayat-builder-logo.svg"
         alt="logo"
         width={160}
         height={50}
