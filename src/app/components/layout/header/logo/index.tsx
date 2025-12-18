18406f881944ee3266fefc60a2d4bb8c7246a38b
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-12-18 17:25:38 +0500

    hero section update

--- src/app/components/layout/header/logo/index.tsx
+++ src/app/components/layout/header/logo/index.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC = () => {
   return (
     <Link href="/">
       <Image
-        src="/images/logo/logo-white.svg"
+        src="/images/logo/logo.svg"
         alt="logo"
         width={160}
         height={50}
